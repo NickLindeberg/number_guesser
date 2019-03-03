c9be4db3b17fe2fae8b3279246a5bcd3046fbfa4
Nick Lindeberg <lindebergnick@gmail.com>
2019-03-03 14:23:17 -0700

    initial setup.  basic page layout with input box and clear button that works!

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+function clearGuess () {
+  // sets a function to clear the guess to use for the clear button
+  document.getElementById("guessForm").reset();
+  // searches page for an id called guessForm and clears the form
+}
