25da70d088ce8121bd60dd260ab1c67d1d5a87a5
Nick Lindeberg <lindebergnick@gmail.com>
2019-03-03 17:24:40 -0700

    refactors user result function to render multiple things at once

--- script.js
+++ script.js
@@ -6,28 +6,26 @@ function clearGuess() {
 
 function showGuess() {
   // creates a function that looks for a user guess and displays it
-  document.getElementById("showGuess").innerHTML =
+  document.getElementById("guess").innerHTML =
   // finds an id called showGuess and populates it with below
-
     document.getElementById("userGuess").value;
     // takes the user input and displays it
 }
 
-function userResult(id) {
+function userResult() {
   // sets a function that takes an id and toggles on or off
-  var x = document.getElementById(id);
+  var x = document.getElementById('information');
   // sets a variable based on id passed into function
-  if (x.style.display === "block") {
-    // sets an if statement that if the display is set to block it wont render
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-    // unless the button has been clicked
-  }
+   x.style.display = "block";
+   showGuess();
 }
 
 function resetPage() {
   // sets a function called resetpage for my reset button
   location.reload();
   // location reload refreshes the page
+}
+
+function disableButton() {
+
 }
